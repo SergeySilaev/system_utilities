7005d23f753fea499817ad4770e782a37cf119b9
Sergey Silaev <SilaevSergeyVladimirovich@gmail.com>
2013-04-24 15:14:11 +0300

    added msvc2010_win64 project creating script

---
+++ _msvc_gen_2010_64_release.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set SOLUTION_NAME=system_utilities
+
+set BOOST_ROOT=d:\usr\boost_1_49_0_win64
+
+set BUILD_TYPE=Release
+
+set BUILD_FOLDER=_build_%BUILD_TYPE%_64
+
+if not exist %BUILD_FOLDER% (
+	mkdir %BUILD_FOLDER%
+)
+
+cd %BUILD_FOLDER%
+cmake -DVERBOSE=OFF -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DSOLUTION_NAME=%SOLUTION_NAME%  -G "Visual Studio 10 Win64" ../ 
+cd ../
+
+echo "%BUILD_FOLDER%/%SOLUTION_NAME%.sln" > _start_msvc_%BUILD_TYPE%.bat
